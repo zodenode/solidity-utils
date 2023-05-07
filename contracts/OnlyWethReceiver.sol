802953bccce75f92f3ab53b339c30d764f47c27f
Edozie I <112929878+zodenode@users.noreply.github.com>
2023-05-07 13:05:13 +0400

    Update OnlyWethReceiver.sol

--- contracts/OnlyWethReceiver.sol
+++ contracts/OnlyWethReceiver.sol
@@ -1,5 +1,9 @@
+/**
+ * @title OnlyWethReceiver
+ * @dev Abstract contract that extends the EthReceiver contract and only allows receiving ETH from a specific contract
+ */
+ 
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 import "./EthReceiver.sol";
@@ -7,10 +11,17 @@ import "./EthReceiver.sol";
 abstract contract OnlyWethReceiver is EthReceiver {
     address private immutable _WETH; // solhint-disable-line var-name-mixedcase
 
+    /**
+     * @dev Constructor that sets the address of the WETH contract
+     * @param weth The address of the WETH contract
+     */
     constructor(address weth) {
         _WETH = address(weth);
     }
 
+    /**
+     * @dev Overrides the receive function of the EthReceiver contract to only allow receiving ETH from the WETH contract
+     */
     function _receive() internal virtual override {
         if (msg.sender != _WETH) revert EthDepositRejected();
     }
