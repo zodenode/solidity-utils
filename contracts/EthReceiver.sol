7fb78beb645c0f0a6e25ee9c6b61d36401f8b702
Edozie I <112929878+zodenode@users.noreply.github.com>
2023-05-07 13:01:20 +0400

    Update EthReceiver.sol
    Added Natspec

--- contracts/EthReceiver.sol
+++ contracts/EthReceiver.sol
@@ -1,16 +1,29 @@
+/**
+ * @title EthReceiver
+ * @dev A contract that can receive Ether deposits and reject them if they were not sent from a contract account
+ */
+ 
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 abstract contract EthReceiver {
+    
+    /**
+     * @dev Emitted when a deposit is rejected because it was not sent from a contract account
+     */
     error EthDepositRejected();
-
+    
+    /**
+     * @dev Fallback function that is called when a contract receives Ether
+     */
     receive() external payable {
         _receive();
     }
 
+    /**
+     * @dev Internal function to receive Ether deposits and reject them if they were not sent from a contract account
+     */
     function _receive() internal virtual {
-        // solhint-disable-next-line avoid-tx-origin
         if (msg.sender == tx.origin) revert EthDepositRejected();
     }
 }
